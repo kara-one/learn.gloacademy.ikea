51404cf046c32b0b0f0720e100ceb05339ea5b9e
Serhii Karavashkin <ksawl@narod.ru>
2020-09-29 22:30:00 +0300

    day 1 / without homework

--- script/main.js
+++ script/main.js
@@ -1,11 +1,16 @@
+'use strict';
 // day1
 
 const btnBurger = document.querySelector('.btn-burger');
 const btnClose = document.querySelector('.btn-close');
-const overlay = document.querySelector('.overlay');
+const btnReturn = document.querySelector('.btn-return');
 const catalog = document.querySelector('.catalog');
 const subCatalog = document.querySelector('.subcatalog');
 const subCatalogHeader = document.querySelector('.subcatalog-header');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () => {
     catalog.classList.add('open');
@@ -13,30 +18,33 @@ const openMenu = () => {
 };
 
 const closeMenu = () => {
+    closeSubMenu();
     catalog.classList.remove('open');
     overlay.classList.remove('active');
 };
 
 const openSubMenu = event => {
     event.preventDefault();
-    const target = event.target;
-    const itemList = target.closest('.catalog-list__item');
-
+    const itemList = event.target.closest('.catalog-list__item');
     if (itemList) {
         subCatalogHeader.innerHTML = itemList.innerHTML;
         subCatalog.classList.add('subopen');
     }
-    console.log('event: ', itemList);
-}
+};
+
+const closeSubMenu = event => {
+    subCatalog.classList.remove('subopen');
+};
 
 btnBurger.addEventListener('click', openMenu);
 catalog.addEventListener('click', openSubMenu);
 
 btnClose.addEventListener('click', closeMenu);
+btnReturn.addEventListener('click', closeSubMenu);
 overlay.addEventListener('click', closeMenu);
 document.addEventListener('keydown', e => {
     if (e.code === 'Escape') {
         closeMenu();
-    }
-    
+        closeSubMenu();
+    }    
 })
