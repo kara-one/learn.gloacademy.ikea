efde892245e932000c4b99e79a41ec174d611860
Serhii Karavashkin <ksawl@narod.ru>
2020-10-02 17:00:55 +0300

    day 3 / finished homework

--- script/generateGoodsPage.js
+++ script/generateGoodsPage.js
@@ -1,6 +1,7 @@
 import {getData} from './getData.js';
 
 const wishList = ['idd005', 'idd008', 'idd078', 'idd045'];
+let blankListMessage = '';
 
 const generateGoodsPage = () => {
 
@@ -8,16 +9,17 @@ const generateGoodsPage = () => {
     const goodsList = document.querySelector('.goods-list');
 
     const generateCards = (data) => {
-        goodsList.textContent = '';
+        let listHTML = '';
 
+        // Each query result-list
         data.forEach(item => {
-            goodsList.insertAdjacentHTML('afterbegin', `
+            listHTML += `
                 <li class="goods-list__item">
 					<a class="goods-item__link" href="card.html#idd001">
 						<article class="goods-item">
 							<div class="goods-item__img">
 								<img src="${item.img[0]}"
-									 data-second-image="${item.img[1]}" alt="${item.name}">
+									data-second-image="${item.img[1]}" alt="${item.name}">
 							</div>
 							<p class="goods-item__new">Новинка</p>
 							<h3 class="goods-item__header">${item.name}</h3>
@@ -30,31 +32,40 @@ const generateGoodsPage = () => {
 						</article>
 					</a>
 				</li>
-            `);
+            `;
         });
+
+        // IF result-list is empty
+        if (listHTML === '') {
+            listHTML = '<li>' + blankListMessage + '</li>';
+        }
+
+        // add view
+        goodsList.textContent = '';
+        goodsList.insertAdjacentHTML('afterbegin', listHTML);
     };
 
+    // Query routes
     if (location.pathname.includes('goods') && location.search) {
         const search = decodeURI(location.search);
         const prop = search.split('=')[0].substring(1);
-        const value = search.split('=')[1];
+        let value = search.split('=')[1];
 
         if (prop === 's') {
-            getData.search(value, generateCards);
+            if (value.includes("+")) value = value.split("+").join(" ");
+
             mainHeader.textContent = `Поиск: ${value}`;
-
-            console.log('goodsList: ', goodsList.textContent === '');
-            if (goodsList.textContent === '') {
-                goodsList.insertAdjacentHTML('afterbegin', '<li>По вашему запросу ничего не найдено!</li>');
-            }
+            blankListMessage = 'По вашему запросу ничего не найдено!';
+            getData.search(value, generateCards);
         } else if (prop === 'wishlist') {
-            getData.wishList(wishList, generateCards);
             mainHeader.textContent = `Список желаний`;
+            blankListMessage = 'Ваш список желаний пуст!';
+            getData.wishList(wishList, generateCards);
         } else if (prop === 'cat' || prop === 'subcat') {
-            getData.category(prop, value, generateCards);
             mainHeader.textContent = value;
+            blankListMessage = 'В этой категории пусто!';
+            getData.category(prop, value, generateCards);
         }
-
     }
 };
 
